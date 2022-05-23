3ba9ed51f56d2366165f450058365b4e4712e1cc
dyo999 <dyoalex@gmail.com>
2022-05-23 22:46:06 +0300

    conftest_setup_and_add_func_test_item

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+
+def test_item(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, ".btn.btn-add-to-basket"), "No have Add button"
+
+
